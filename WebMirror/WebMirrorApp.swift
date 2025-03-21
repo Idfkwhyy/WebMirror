4464b7bf17924ea8413e9002f283b256cece7014
Leonardo Munarolo <leonardodm510@gmail.com>
2025-03-21 17:43:43 -0300

    Initial Commit

---
+++ WebMirror/WebMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebMirrorApp.swift
+//  WebMirror
+//
+//  Created by Leonardo Munarolo on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WebMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebMirrorTests/WebMirrorTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebMirrorTests.swift
+//  WebMirrorTests
+//
+//  Created by Leonardo Munarolo on 21/03/25.
+//
+
+import Testing
+@testable import WebMirror
+
+struct WebMirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
